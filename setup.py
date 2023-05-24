a187c839c92f93a604c058b8cecc5a36e35c3bc3
rbeker <rbeker@bmc.com>
2023-05-24 14:20:26 +0300

    + new version release 0.0.4 using pytest

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'A conversion package'
 LONG_DESCRIPTION = 'A package that makes it easy to convert values between several units of measurement'
 
