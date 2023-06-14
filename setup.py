c13035b5b01ed497ed0200932768db1264464cf1
rbeker <rbeker@bmc.com>
2023-06-14 10:45:50 +0300

    + new version release 0.1.23

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.22'
+VERSION = '0.1.23'
 DESCRIPTION = 'A conversion package'
 LONG_DESCRIPTION = 'A package that makes it easy to convert values between several units of measurement'
 
